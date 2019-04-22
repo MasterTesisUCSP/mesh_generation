70df485a92fc9ad95ebaaf441e0c8e37081933fb
fil <fvilca@gmail.com>
2019-04-22 18:24:22 -0500

    ,

---
+++ B-Mesh/main.cpp
@@ -0,0 +1,44 @@
+//
+//  main.cpp
+//  B-Mesh
+//
+//  Created by Bryan Gonzales Vega on 6/14/16.
+//  Copyright © 2016 Bryan Gonzales Vega. All rights reserved.
+//
+
+//#define PROCESSING_TYPE OpenCL
+#define DEBUG_MODE
+
+#if defined(__APPLE__) || defined(__MACOSX)
+    #define IMAGEMAGICK_PATH "/usr/local/bin/convert"
+    #define RESULTS_PATH "Results/"
+    #define PROJECT_PATH ""
+    #define ASSETS_PATH PROJECT_PATH "Assets/"
+    #define KERNELS_PATH PROJECT_PATH ""
+#else
+    #define IMAGEMAGICK_PATH "/usr/bin/convert"
+    #define RESULTS_PATH PROJECT_PATH "Results/"
+    #define PROJECT_PATH "/home/fil/Documentos/MeshGeneration/B-Mesh/"
+    #define ASSETS_PATH PROJECT_PATH "Assets/"
+    #define KERNELS_PATH PROJECT_PATH "BMesh/Processing/OpenCL/"
+#endif
+
+#include <sys/stat.h>
+#include "MeshGeneration.h"
+
+int main() {
+    std::setbuf(stdout, NULL);
+    std::vector<TrialPointer> experiments {
+        BUILD_TRIAL(kMesh1),
+        //BUILD_TRIAL(Taz),
+    };
+    
+    for( auto trial : experiments ){
+        const std::string path = RESULTS_PATH + trial->identifier + "/";
+        mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH );
+
+        meshGeneration(trial, path);
+    }
+
+    return 0;
+}
